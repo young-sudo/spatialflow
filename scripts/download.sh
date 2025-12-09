591b568078d2f14ec45c2422d08771b61768481b
young-sudo <91847945+young-sudo@users.noreply.github.com>
2025-12-09 15:46:27 +0100

    Add download script

---
+++ scripts/download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+!wget  https://cf.10xgenomics.com/supp/spatial-exp/analysis-workshop/multisample_raw_data.tar.gz
+!tar xvzf multisample_raw_data.tar.gz
+!rm multisample_raw_data.tar.gz
